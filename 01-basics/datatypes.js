a3dddecbf08e0c5ba321c69355907613018296b8
SahilNaseer <156059830+SahilNaseer@users.noreply.github.com>
2024-01-09 09:10:25 +0000

    datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,14 @@
+" use strict"; //treat newwer version of js
+
+//alart () we are using node js not browser
+
+let name = 'mahad ' //string
+let age = 18 //number 
+let login = true //bolen  (5 types of famous data types)
+let me = null  //is a data type
+// symbol is a data type = unique
+
+
+//object
+
+console.log(tyoeof "i am khan ");

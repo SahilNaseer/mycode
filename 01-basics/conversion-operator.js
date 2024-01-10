25727ee6c368a3b8c521cf40af1f68df35341ce5
SahilNaseer <156059830+SahilNaseer@users.noreply.github.com>
2024-01-10 06:22:10 +0000

    converstion of data

---
+++ 01-basics/conversion-operator.js
@@ -0,0 +1,12 @@
+let score = 40 ;
+console.log( score );
+
+
+let isLoggedIn = "";
+
+let islogin = Boolean (isLoggedIn)
+
+console.log(islogin);
+
+//empty value of var is detected as false (of boolean)
+//a common name as (boolean ) is detected as true 
